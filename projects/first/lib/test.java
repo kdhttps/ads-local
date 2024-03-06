3920b4215225bd8616b3d363aa2c933fd5799d0b
Kiran Mali <39133739+kdhttps@users.noreply.github.com>
2024-03-06 19:42:23 +0530

    feat: update test.java

--- projects/first/lib/test.java
+++ projects/first/lib/test.java
@@ -1,1 +1,14 @@
-java code
+class HelloWorld {
+    
+    public String generateSecretKey(int keyLen) throws NoSuchAlgorithmException {
+        byte[] randomBytes = new byte[keyLen];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(randomBytes);
+
+        StringBuilder result = new StringBuilder();
+        for (byte b : randomBytes) {
+            result.append(String.format("%02X", b));
+        }
+        return result.toString();
+    }
+}
